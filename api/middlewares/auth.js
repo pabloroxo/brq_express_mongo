4eeef3ed39ca8dc4cbc74f8deb36755003b3afcf
Pablo Roxo <pabloricardoroxosilva@gmail.com>
2021-07-16 00:30:41 -0300

    Melhoria na mensagem de erro do token do middleware de auth

--- api/middlewares/auth.js
+++ api/middlewares/auth.js
@@ -5,22 +5,34 @@ module.exports = (req, res, next) => {
     const token = req.headers.authorization;
 
     if (!token) {
-        return res.status(401).send({ error: 'Token não informado'});
+        return res.status(401).send({ error: 'Token não informado' });
     }
 
     const partes = token.split(' ');
 
     if (partes.length !== 2) {
-        return res.status(401).send({ error: 'O token é inválido (e1)'});
+        return res.status(401).send({ error: 'O token é inválido (e1)' });
     }
 
     if (partes[0] !== 'Bearer') {
-        return res.status(401).send({ error: 'O token é inválido (e2)'});
+        return res.status(401).send({ error: 'O token é inválido (e2)' });
     }
 
     jwt.verify(partes[1], authConfig.secret, (err, decoded) => {
+        let error;
         if (err) {
-            return res.status(401).send({ error: 'O token é inválido (e3)', token: token});
+            switch (err.name) {
+                case 'JsonWebTokenError':
+                        error = 'O token é inválido (e3)';
+                    break;
+                case 'TokenExpiredError':
+                        error = 'O token está expirado';
+                    break;
+                default:
+                        error = 'Erro com o token: ' + err.name + ' - ' + err.message;
+                    break;
+            }
+            return res.status(401).send({ error: error });
         }
 
         req.id = decoded.id;
